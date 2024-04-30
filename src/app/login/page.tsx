2259effc591c7e4b9726e9bedc83f7b4d5861f1c
RASZZZKING <farras.akhirio.ramadhan.204@gmail.com>
2024-04-30 15:53:22 +0700

    isi login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { FunctionComponent } from "react";
+
+interface pageProps {
+    
+}
+ 
+const page: FunctionComponent<pageProps> = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default page;
